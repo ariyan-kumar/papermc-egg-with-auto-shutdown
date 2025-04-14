75fc3e258a4536d6043d9847ff4758a95f2b87cf
Ariyan Kumar <ariyan.kumar.xd@gmail.com>
2025-04-14 22:58:43 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
-rm -f server.jar && curl -o server.jar https://api.papermc.io/v2/projects/paper/versions/1.20.1/builds/123/downloads/paper-1.20.1-123.jar && java -Xms128M -XX:MaxRAMPercentage=95.0 -Dterminal.jline=false -Dterminal.ansi=true -jar server.jar
+:: Remove the existing server.jar file
+del /f server.jar
+
+:: Download the new server.jar file
+curl -o server.jar https://api.papermc.io/v2/projects/paper/versions/1.20.1/builds/123/downloads/paper-1.20.1-123.jar
+
+:: Run the server.jar file with Java
+java -Xms128M -XX:MaxRAMPercentage=95.0 -Dterminal.jline=false -Dterminal.ansi=true -jar server.jar
