231fdf2f05fe3739b5d0db50b24aac10dea88e2b
Ariyan Kumar <ariyan.kumar.xd@gmail.com>
2025-04-14 23:03:51 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
-:: Remove the existing server.jar file
-del /f server.jar
+#!/bin/bash
 
-:: Download the new server.jar file
+# Remove the old server.jar if it exists
+rm -f server.jar
+
+# Download the latest PaperMC jar
 curl -o server.jar https://api.papermc.io/v2/projects/paper/versions/1.20.1/builds/123/downloads/paper-1.20.1-123.jar
 
-:: Run the server.jar file with Java
-java -Xms128M -XX:MaxRAMPercentage=95.0 -Dterminal.jline=false -Dterminal.ansi=true -jar server.jar
+# Start the Minecraft server
+java -Xms128M -XX:MaxRAMPercentage=95.0 -Dterminal.jline=false -Dterminal.ansi=true -jar server.jar nogui
